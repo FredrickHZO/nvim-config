3a4bdc72eb12c62cf0806da837715557da8b434e
FredrickHZO <federico.armagno97@gmail.com>
2024-01-02 15:12:57 +0100

    Initial commit

---
+++ init.lua
@@ -0,0 +1,19 @@
+require("federicoarmagno.plugins-setup")
+require("federicoarmagno.plugins.lsp") 
+require("federicoarmagno.plugins.treesitter")
+require("federicoarmagno.core.options")
+require("federicoarmagno.core.keymaps")
+require("federicoarmagno.core.colorscheme")
+require("federicoarmagno.core.telescope")
+require("federicoarmagno.plugins.undotree")
+require("federicoarmagno.plugins.feline")
+
+vim.diagnostic.config({
+     signs = true,
+     underline = true,
+     update_in_insert = true,
+     -- virtual_text = false, 
+     virtual_text = {
+          prefix = '●', -- Could be '●', '▎', 'x', '■' 
+     }
+ })
